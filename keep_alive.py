5d71606f51b1d770fd64e9082fb7dac8b5f5ce89
Luisfer Doria <81895918+luis123doria@users.noreply.github.com>
2021-11-28 00:05:34 -0400

    Create keep_alive.py
    This code is for Keep Alive the Discord Bot, pinging the server every 5 minutes

---
+++ keep_alive.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+#url generated: https://Clustrsoul-Discord-Bot.luis123doria.repl.co
+
+@app.route('/')
+def home():
+    return "Hello. I am alive!"
+
+def run():
+    app.run(host='0.0.0.0',port=8000)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+    
+    
